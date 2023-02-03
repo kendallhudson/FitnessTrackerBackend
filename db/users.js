7d9cbb36c2bb1a195f4d9b641b0745d77cc839f2
Kim-Bim-123 <113792671+Kim-Bim-123@users.noreply.github.com>
2023-02-02 20:54:49 -0500

    Update users.js

--- db/users.js
+++ db/users.js
@@ -7,7 +7,7 @@ async function createUser({ username, password }) {
   try {
     console.log("Creating USERS to use")
   } catch (error) {
-    
+
   }
 }
 
@@ -16,10 +16,38 @@ async function getUser({ username, password }) {
 }
 
 async function getUserById(userId) {
+  try {
+    console.log("Getting User By ID")
+    const { rows: [ user ]} = await client.query(`
+    SELECT *
+    FROM users
+    WHERE id=${id}
+    `);
 
+  if (!user) {
+    return null
+  } else {
+    delete user.password;
+    return user;
+  }
+} catch (error) {
+  console.log("Error Getting User by ID")
+  throw error;
+  }
 }
 
 async function getUserByUsername(userName) {
+try {
+  console.log("Getting User By Username")
+  const { rows: [ user ] } = await client.query(`
+  SELECT *
+  FROM username
+  WHERE user=${ user }
+  `)
+} catch (error) {
+  console.log("Error Getting User by Username")
+  throw error;
+  }
 
 }
 
