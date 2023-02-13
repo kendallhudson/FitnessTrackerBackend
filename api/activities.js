e1a2ccb9881c885143976faab25fd380fb58b87e
Kendall Hudson <64994077+kendallhudson@users.noreply.github.com>
2023-02-12 22:05:49 -0600

    most activities.js api routes completed

--- api/activities.js
+++ api/activities.js
@@ -1,28 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
+const { requireUser } = require("./utils");
+
 const { 
     createActivity,
-    getAllActivities
+    getAllActivities,
+    getPublicRoutinesByActivity
  } = require("../db");
-
-function requireUser(req, res, next) {
-    if (!req.user) {
-      next({
-        name: "MissingUserError",
-        message: "You must be logged in to perform this action"
-      });
-    }
-  
-    next();
-  }
-  
-  module.exports = {
-    requireUser
-  }
 
 
 // GET /api/activities/:activityId/routines
+router.use((req, res, next) => {
+    console.log("A request is being made to /activities/:activityId/routines");
+  
+    next(); 
+  });
+  
+router.get("/activities/:activityId/routines", async (req, res) => {
+    const activities = await getPublicRoutinesByActivity();
+   
+    res.send({
+      activities,
+    });
+  });
 
 // GET /api/activities
 router.use((req, res, next) => {
@@ -31,14 +32,40 @@ router.use((req, res, next) => {
     next(); 
   });
   
-router.get("/", async (req, res) => {
+router.get("/activities", async (req, res) => {
     const activities = await getAllActivities();
    
     res.send({
       activities,
     });
   });
+
 // POST /api/activities
+router.post("/activities", requireUser, async (req, res, next) => {
+
+    const { name, description } = req.body;
+  
+    try {
+      
+      const activities = await createActivity(name, description);
+      // this will create the activity for us
+  
+      if (activities) {
+        res.send(activities);
+        // if the activity comes back, res.send({ activity });
+  
+      } else {
+        next({
+          name: 'ActivityDataError',
+          message: `Activity not created properly`
+        });
+        // otherwise, next an appropriate error object
+      }
+  
+    } catch ({ name, message }) {
+      next({ name, message });
+    }
+  });
 
 // PATCH /api/activities/:activityId
 
